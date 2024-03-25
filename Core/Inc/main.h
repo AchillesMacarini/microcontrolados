6a9cb209953d1a79a6c75cf52c7bc7d63d166187
AchillesMacarini <capivaradoacre@gmail.com>
2024-03-25 19:13:55 -0300

    Atualização de arquivos para roteiro_01.

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -59,6 +59,19 @@ void Error_Handler(void);
 /* Private defines -----------------------------------------------------------*/
 #define KIT_LED_Pin GPIO_PIN_13
 #define KIT_LED_GPIO_Port GPIOC
+#define LED1_Pin GPIO_PIN_3
+#define LED1_GPIO_Port GPIOA
+#define LED2_Pin GPIO_PIN_4
+#define LED2_GPIO_Port GPIOA
+#define LED3_Pin GPIO_PIN_5
+#define LED3_GPIO_Port GPIOA
+#define LED4_Pin GPIO_PIN_6
+#define LED4_GPIO_Port GPIOA
+#define LED5_Pin GPIO_PIN_7
+#define LED5_GPIO_Port GPIOA
+#define BTN1_Pin GPIO_PIN_15
+#define BTN1_GPIO_Port GPIOB
+
 /* USER CODE BEGIN Private defines */
 
 /* USER CODE END Private defines */
